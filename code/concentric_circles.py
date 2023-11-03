5511d43a2c71799bb67f77dde017ee5d8d3e72b6
James Lemin <jameslemin@duck.com>
2023-11-03 17:13:34 +0000

    concentrice circles

--- code/concentric_circles.py
+++ code/concentric_circles.py
@@ -1,144 +1,58 @@
-import sys
-import datetime
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import random
-# create svg file of concentric circles using python
-A4 = (210, 297)
-A3 = (297, 420)
-ppmm = 5  # pixels per mm
+import toml
 
-STROKE_COLOUR = "black"
-STROKE_WIDTH = ppmm
-FILL_COLOUR = "none"
-NOISE = 0.05
+# local libraries
+import svg
+import draw  # noqa: F401
+import utils
 
-DEFAULT_PAPER_SIZE = A3
+# Load config file
+config = toml.load('config.toml')
+
+# Paper sizes and pixels
+DEFAULT_SIZE = config['paper_sizes']['A3']
 DEFAULT_LANDSCAPE = True
-DEFAULT_CIRCLES = 80
-DEFAULT_PIXELS_PER_MM = ppmm
+DEFAULT_PPMM = config['page']['pixels_per_mm']
+DEFAULT_BLEED = config['page']['bleed']
+
+DEFAULT_OUTPUT_DIR = config['directories']['output']
+
+# Stroke and fill colours
+STROKE_COLOUR = config['colours']['stroke']
+STROKE_WIDTH = config['page']['pixels_per_mm']
+FILL_COLOUR = config['colours']['fill']
+
+paper_size = svg.set_image_size(DEFAULT_SIZE, DEFAULT_PPMM, DEFAULT_LANDSCAPE)
+drawable_area = svg.set_drawable_area(paper_size, DEFAULT_BLEED)
+# set filename, creating output directory if necessary
+filename = utils.create_dir(DEFAULT_OUTPUT_DIR) + utils.generate_filename()
+utils.print_params({"paper_size": paper_size,
+                    "drawable_area": drawable_area,
+                    "filename": filename})
+
+# LOCAL VARIABLES
+NOISE = 0.05
+CIRCLE_COUNT = 80
+# LOCAL FUNCTIONS
 
 
-def set_landscape(paper_size, landscape):
-    """
-    Sets the landscape orientation of a paper size.
-
-    Args:
-      paper_size (tuple): A tuple containing the dimensions of the paper size
-      in the form (width, height).
-      landscape (bool): A boolean value indicating whether the paper should be
-      in landscape orientation.
-
-    Returns:
-      tuple: A tuple containing the dimensions of the paper size in the form
-      (width, height), with the dimensions swapped if landscape is True.
-    """
-    if landscape:
-        return (paper_size[1], paper_size[0])
-    else:
-        return paper_size
-
-
-def set_image_size(paper_size, ppmm):
-    """
-    Calculates the size of the image in pixels based on the paper size and
-    pixels per millimeter.
-
-    Args:
-      paper_size (tuple): A tuple containing the width and height of the paper
-      in millimeters.
-      ppmm (float): The number of pixels per millimeter.
-
-    Returns:
-      tuple: A tuple containing the width and height of the image in pixels.
-    """
-    return (int(paper_size[0] * ppmm), int(paper_size[1] * ppmm))
-
-
-def set_canvas_size(image_size):
-    """
-    Calculates the size of the canvas for concentric circles based on the
-    given image size.
-
-    Args:
-      image_size (tuple): A tuple containing the width and height of the image.
-
-    Returns:
-      tuple: A tuple containing the width and height of the canvas for
-      concentric circles.
-    """
-    return (int(image_size[0] * 0.95), int(image_size[1] * 0.95))
-
-
-# def set_svg_background(paper_size):
-#     """ set the background colour """
-#     return """<rect width="{}" height="{}" style="fill:{}" />""".format(
-#         paper_size[0], paper_size[1], background_color)
-
-
-def centre_canvas(paper_size, canvas_size):
-    """ centre the canvas on the page """
-    pos_x = int((paper_size[0] - canvas_size[0]) / 2)
-    pos_y = int((paper_size[1] - canvas_size[1]) / 2)
-    return (pos_x, pos_y)
-
-
-def svg_header(paper_size, canvas_size):
-    """
-    Returns an SVG header string with the specified paper and canvas sizes.
-
-    Args:
-      paper_size (tuple): A tuple containing the width and height of the paper.
-      canvas_size (tuple): A tuple containing the width and height of the
-      canvas.
-
-    Returns:
-      str: An SVG header string with the specified paper and canvas sizes.
-    """
-    offset_x, offset_y = centre_canvas(paper_size, canvas_size)
-    xml1 = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>"""
-    xml2 = """<svg width="{}" height="{}" viewBox="{} {} {} {}" """.format(
-        paper_size[0], paper_size[1],
-        offset_x, offset_y, canvas_size[0], canvas_size[1])
-    xml3 = """xmlns="http://www.w3.org/2000/svg" version="1.1">"""
-    return xml1 + "\n" + xml2 + "\n" + xml3 + "\n"
-
-
-def svg_footer():
-    return "</svg>"
-
-
-def draw_circle(cx, cy, r):
-    """
-    Returns an SVG circle element as a string with the specified center
-    coordinates and radius.
-
-    Args:
-      cx (float): The x-coordinate of the center of the circle.
-      cy (float): The y-coordinate of the center of the circle.
-      r (float): The radius of the circle.
-
-    Returns:
-      str: An SVG circle element as a string.
-    """
-    circle_def = """<circle cx="{}" cy="{}" r="{}" """.format(cx, cy, r)
-    circle_style = """stroke="{}" stroke-width="{}" fill="{}" />""".format(
-        STROKE_COLOUR, STROKE_WIDTH, FILL_COLOUR)
-    return circle_def + "\n" + circle_style + "\n"
-
-
-def calculate_max_radius(canvas_size):
+def calculate_max_radius(drawable_area):
     """
     Calculates the maximum radius that can be used for concentric circles on a
     canvas of the given size.
 
     Args:
-      canvas_size (tuple): A tuple containing the width and height of the
+      drawable_area (tuple): A tuple containing the width and height of the
       canvas.
 
     Returns:
       int: The maximum radius that can be used for concentric circles on the
       canvas.
     """
-    return min(canvas_size)
+    return min(drawable_area[3] - drawable_area[1],
+               drawable_area[2] - drawable_area[0]) * 0.45
 
 
 def weighted_random(weight):
@@ -146,23 +60,25 @@ def weighted_random(weight):
     return random.random()*weight
 
 
-def skew_centre(canvas_size):
+def skew_centre(drawable_area):
     """Returns a tuple with the coordinates of the centre of the canvas,
     skewed by a random amount.
 
     Args:
-      canvas_size (tuple): A tuple with the width and height of the canvas.
+      drawable_area [list]: list of min_x, min_y, max_x, max_y
 
     Returns:
       tuple: A tuple with the x and y coordinates of the skewed centre of the
       canvas.
     """
-    skewed_x = canvas_size[0] * (1+weighted_random(NOISE)) / 2
-    skewed_y = canvas_size[1] * (1+weighted_random(NOISE)) / 2
+    skewed_x = drawable_area[0] + \
+        (drawable_area[2] - drawable_area[0]) * (1+weighted_random(NOISE)) / 2
+    skewed_y = drawable_area[1] + \
+        (drawable_area[3] - drawable_area[1]) * (1+weighted_random(NOISE)) / 2
     return (int(skewed_x), int(skewed_y))
 
 
-def generate_circle_list(paper_size, circle_count):
+def generate_circle_list(drawable_area, circle_count):
     """
     Generate a list of circle radii.
 
@@ -174,74 +90,25 @@ def generate_circle_list(paper_size, circle_count):
       list: A list of circle radii.
 
     """
-    canvas_size = set_canvas_size(paper_size)
-    max_radius = calculate_max_radius(canvas_size)
+    max_radius = calculate_max_radius(drawable_area)
     circle_list = []
     for i in range(circle_count):
-        circle_list.append(
-            int(max_radius * ((i+i*weighted_random(NOISE)) / circle_count)))
+        circle = int(max_radius * ((i+i*weighted_random(NOISE))
+                                   / circle_count))
+        if circle < max_radius:
+            circle_list.append(circle)
+        else:
+            circle_list.append(max_radius)
+            
     return circle_list
 
 
-def generate_filename():
-    """Generates a filename for the SVG file based on the name of the script,
-    the current date, and the current time.
+svg_list = []
+# fill svg_list with svg objects
+for i in generate_circle_list(drawable_area, CIRCLE_COUNT):
+    skew_x, skew_y = skew_centre(drawable_area)
+    svg_list.append(draw.circle(skew_x, skew_y, i, STROKE_COLOUR,
+                                STROKE_WIDTH, FILL_COLOUR))
 
-    Returns:
-      str: The filename in the format "<name of script>_<date>_<time>.svg".
-    """
-    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    return "{}_{}.svg".format(sys.argv[0], timestamp)
-
-
-def write_svg_file(filename, svg_list):
-    """
-    Write an array of SVG lines to a file.
-
-    Args:
-      filename (str): The name of the file to write to.
-      svg_list (list): A list of strings representing SVG lines.
-
-    Returns:
-      None
-    """
-    with open(filename, "w") as f:
-        for line in svg_list:
-            f.write(line)
-# main ###
-
-
-def create_svg_list(paper_size, circle_count):
-    """
-    Create a list of SVG elements representing concentric circles.
-
-    Args:
-      paper_size (tuple): A tuple of two integers representing the width and
-      height of the paper in millimeters.
-      circle_count (int): The number of concentric circles to draw.
-
-    Returns:
-      list: A list of SVG elements representing the concentric circles.
-    """
-    image_size = set_image_size(paper_size, ppmm)
-    canvas_size = set_canvas_size(image_size)
-    svg_list = []
-    svg_list.append(svg_header(image_size, canvas_size))
-    for i in generate_circle_list(image_size, circle_count):
-        skew_x, skew_y = skew_centre(canvas_size)
-        svg_list.append(draw_circle(skew_x, skew_y, i))
-    svg_list.append(svg_footer())
-    return svg_list
-
-
-def main():
-    file_name = generate_filename()
-    paper_size = set_image_size(DEFAULT_PAPER_SIZE, DEFAULT_PIXELS_PER_MM)
-    paper_size = set_landscape(paper_size, DEFAULT_LANDSCAPE)
-    svg_list = create_svg_list(
-        paper_size, DEFAULT_CIRCLES)
-    write_svg_file(file_name, svg_list)
-
-
-if __name__ == "__main__":
-    main()
+doc = svg.build_svg_file(paper_size, drawable_area, svg_list)
+svg.write_file(filename, doc)
--- code/draw.py
+++ code/draw.py
@@ -20,9 +20,37 @@ def box(start_xy, box_size, stroke_colour, stroke_width, fill_colour):
         position, size, stroke, fill)
 
 
+def circle(cx, cy, r, stroke_colour, stroke_width, fill_colour):
+    """
+    Returns an SVG circle element as a string with the specified center
+    coordinates and radius.
+
+    Args:
+      cx (float): The x-coordinate of the center of the circle.
+      cy (float): The y-coordinate of the center of the circle.
+      r (float): The radius of the circle.
+
+    Returns:
+      str: An SVG circle element as a string.
+    """
+    circle_def = """<circle cx="{}" cy="{}" r="{}" """.format(cx, cy, r)
+    circle_style = """stroke="{}" stroke-width="{}" fill="{}" />""".format(
+        stroke_colour, stroke_width, fill_colour)
+    return circle_def + "\n" + circle_style + "\n"
+
+
 def set_background(drawable_area, background_colour):
     """
     returns a rectangle of background_colour the size of the drawable area
     """
     return box(drawable_area[:2], drawable_area[2] - drawable_area[0],
                background_colour, 0, background_colour)
+
+
+def quadratic_curve(start_xy, control_xy, end_xy, stroke_width, stroke_colour):
+    """ return a quadratic curve """
+    start = """<path d="M {} {} """.format(start_xy[0], start_xy[1])
+    control = """Q {} {} """.format(control_xy[0], control_xy[1])
+    end = """{} {}" stroke="{}" stroke-width="{}" fill="none" />""".format(
+        end_xy[0], end_xy[1], stroke_colour, stroke_width)
+    return start + control + end
